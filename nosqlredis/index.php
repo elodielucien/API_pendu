13c42134d5040de87b1a5f4e2cad7faaef63e104
vinoth1710 <vinoth17@hotmail.fr>
2020-03-18 20:02:12 +0100

    connexion bdd

--- nosqlredis/index.php
+++ nosqlredis/index.php
@@ -9,9 +9,9 @@ try {
 
     $redis = new Predis\Client(array(
         "scheme" => "tcp",
-        "host" => "SG-TPNOSQL-31571.servers.mongodirector.com",//changer le nom de la base
+        "host" => "SG-Pendu-32039.servers.mongodirector.com:6379",//changer le nom de la base
         "port" => 6379,
-        "password"=>"bsXiEYKjq3AfpKRjuuRheT5ig7nZyZuF"//changer le mot de passe de la base
+        "password"=>"THpCYGub1Hlz1mjSF34scGgWWaoyBYr5"//changer le mot de passe de la base
     ));
 
 }
@@ -20,7 +20,7 @@ catch (Exception $e) {
 }
 
 // mise à jour de la valeur
-$redis->set('message', 'Hello world');
+$redis->set('message', 'Coucou');
 
 // recuperation de la valeur
 $value = $redis->get('message');
